08ece675bd38e04f8755e9275b9d900130a021af
SatyamShandilya-github <satyam240407@gmail.com>
2026-03-01 11:55:05 +0530

    Update musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,12 @@
+music = {
+    "shape of you": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "believer": "https://www.youtube.com/watch?v=7wtfhZwyrcc",
+    "unstoppable": "https://www.youtube.com/watch?v=YaEG2aWJnZ8",
+    "calm down": "https://www.youtube.com/watch?v=WcIcVapfqXw",
+    "kesariya": "https://www.youtube.com/watch?v=BddP6PYo2gs",
+    "perfect": "https://www.youtube.com/watch?v=2Vv-BfVoq4g",
+    "srivalli": "https://www.youtube.com/watch?v=txHO7PLGE0M",
+    "levitating": "https://www.youtube.com/watch?v=TUVcZfQe-Kw",
+    "enemy": "https://www.youtube.com/watch?v=D9G1VOjN_84",
+    "hymn for the weekend": "https://www.youtube.com/watch?v=YykjpeuMNEk"
+s}
